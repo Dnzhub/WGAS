fee47d57089c9e2cfec936ddc6f867a8806d46cf
DnzZzZ <47675792+Dnzhub@users.noreply.github.com>
2025-11-18 20:22:13 +0300

    Project initialization

---
+++ Source/WGAS/WGAS.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "WGAS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, WGAS, "WGAS" );
---
+++ Source/WGAS/WGASGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WGASGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WGAS_API AWGASGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
