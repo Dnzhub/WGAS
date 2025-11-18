14548a7b07db6d7692ab3c99624de66b6c110005
DnzZzZ <47675792+Dnzhub@users.noreply.github.com>
2025-11-19 00:14:27 +0300

    Set player and skeleton and weapon meshes

--- Source/WGAS/Private/Character/WCharacterBase.cpp
+++ Source/WGAS/Private/Character/WCharacterBase.cpp
@@ -7,6 +7,9 @@ AWCharacterBase::AWCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 void AWCharacterBase::BeginPlay()
