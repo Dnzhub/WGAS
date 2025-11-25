eb1582a9b894aa4362b056c2733242b6a29a1621
DnzZzZ <47675792+Dnzhub@users.noreply.github.com>
2025-11-25 13:28:46 +0300

    Add player state class

---
+++ Source/WGAS/Private/State/WPlayerState.cpp
@@ -0,0 +1,10 @@
+﻿// Copyright Deniz Yilmaz
+
+
+#include "State/WPlayerState.h"
+
+AWPlayerState::AWPlayerState()
+{
+	//How often server update the clients
+	NetUpdateFrequency = 100.f;
+}
---
+++ Source/WGAS/Public/State/WPlayerState.h
@@ -0,0 +1,18 @@
+﻿// Copyright Deniz Yilmaz
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "WPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WGAS_API AWPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+public:
+	AWPlayerState();
+};
