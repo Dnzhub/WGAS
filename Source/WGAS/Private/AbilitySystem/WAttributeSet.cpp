92d79520ae7fdba1f7531ea185f0af52b48c1c80
DnzZzZ <47675792+Dnzhub@users.noreply.github.com>
2025-11-25 19:03:17 +0300

    Add Health & Mana Attributes
    -> Create boilerplate for Health, MaxHealth, Mana, MaxMana attributes
    -> Replicate those attributes
    -> Add getter, setter and initializer with ATTRIBUTE_ACCESSORS

--- Source/WGAS/Private/AbilitySystem/WAttributeSet.cpp
+++ Source/WGAS/Private/AbilitySystem/WAttributeSet.cpp
@@ -2,3 +2,56 @@
 
 
 #include "AbilitySystem/WAttributeSet.h"
+
+#include "Net/UnrealNetwork.h"
+
+UWAttributeSet::UWAttributeSet()
+{
+	InitHealth(100.f);
+	InitMaxHealth(100.f);
+	InitMana(250.f);
+	InitMaxMana(250.f);
+}
+
+//Register for Replication boilerplate
+void UWAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	/**
+	*REPNOTIFY_Always  even if the new value and old value are both the same, the function will run.
+
+	with REPNOTIFY_OnChangged, it will only run if the newvalue and oldvalue are different
+	 * **/
+	DOREPLIFETIME_CONDITION_NOTIFY(UWAttributeSet,Health,COND_None,REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UWAttributeSet,MaxHealth,COND_None,REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UWAttributeSet,Mana,COND_None,REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UWAttributeSet,MaxMana,COND_None,REPNOTIFY_Always);
+	
+}
+
+
+
+//Called right after health attribute replicated
+void UWAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UWAttributeSet,Health, OldHealth);
+}
+
+void UWAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UWAttributeSet,MaxHealth, OldMaxHealth);
+
+}
+
+void UWAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UWAttributeSet,Mana, OldMana);
+}
+
+void UWAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UWAttributeSet,MaxMana, OldMaxMana);
+
+}
--- Source/WGAS/Public/AbilitySystem/WAttributeSet.h
+++ Source/WGAS/Public/AbilitySystem/WAttributeSet.h
@@ -4,8 +4,17 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
 #include "WAttributeSet.generated.h"
 
+
+//Create Accessor for an attribute(Setter, Getter, Initializer) with macro
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+ 
 /**
  * 
  */
@@ -13,4 +22,45 @@ UCLASS()
 class WGAS_API UWAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+
+public:
+	UWAttributeSet();
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing = OnRep_Health, Category = "Vital Attributes")
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UWAttributeSet,Health)
+
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing = OnRep_MaxHealth, Category = "Vital Attributes")
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UWAttributeSet,MaxHealth)
+
+
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing = OnRep_Mana, Category = "Vital Attributes")
+	FGameplayAttributeData Mana;
+	ATTRIBUTE_ACCESSORS(UWAttributeSet,Mana)
+
+
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing = OnRep_MaxMana, Category = "Vital Attributes")
+	FGameplayAttributeData MaxMana;
+	ATTRIBUTE_ACCESSORS(UWAttributeSet,MaxMana)
+
+
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
+
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
+protected:
+
+	
 };
+
+
