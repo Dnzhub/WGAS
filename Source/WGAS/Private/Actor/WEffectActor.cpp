5ab4839c3c2164a0b891980d19e0fa511c97bdfd
DnzZzZ <47675792+Dnzhub@users.noreply.github.com>
2025-11-26 13:38:25 +0300

    Add effect actor base class
    -> Generate health potion from base effect actor

---
+++ Source/WGAS/Private/Actor/WEffectActor.cpp
@@ -0,0 +1,53 @@
+﻿// Copyright Deniz Yilmaz
+
+
+#include "Actor/WEffectActor.h"
+
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemInterface.h"
+#include "AbilitySystem/WAttributeSet.h"
+#include "Components/SphereComponent.h"
+
+
+AWEffectActor::AWEffectActor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>("StaticMeshComponent");
+	SetRootComponent(StaticMeshComponent);
+	
+	SphereComponent = CreateDefaultSubobject<USphereComponent>("SphereComponent");
+	SphereComponent->SetupAttachment(GetRootComponent());
+}
+
+
+void AWEffectActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AWEffectActor::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+	SphereComponent->OnComponentBeginOverlap.AddDynamic(this, &AWEffectActor::OnOverlap);
+	SphereComponent->OnComponentEndOverlap.AddDynamic(this, &AWEffectActor::EndOverlap);
+}
+
+void AWEffectActor::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyState, bool bFromSweep, const FHitResult& SweepResult)
+{
+	//TODO: Change this to apply a gameplay effect. For now using_const cast but its terrible idea to cast const
+	if (IAbilitySystemInterface* AbilitySystemInterface = Cast<IAbilitySystemInterface>(OtherActor))
+	{
+		const UWAttributeSet* AttributeSet = Cast<UWAttributeSet>(AbilitySystemInterface->GetAbilitySystemComponent()->GetAttributeSet(UWAttributeSet::StaticClass()));
+		UWAttributeSet* MutableAttributeSet = const_cast<UWAttributeSet*>(AttributeSet);
+		MutableAttributeSet->SetHealth(AttributeSet->GetHealth() + 25.f);
+		Destroy();
+	}
+}
+
+void AWEffectActor::EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+}
---
+++ Source/WGAS/Public/Actor/WEffectActor.h
@@ -0,0 +1,35 @@
+﻿// Copyright Deniz Yilmaz
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "WEffectActor.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class WGAS_API AWEffectActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AWEffectActor();
+
+	UFUNCTION()
+	virtual void OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyState, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	virtual void EndOverlap( UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void PostInitializeComponents() override;
+private:
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USphereComponent> SphereComponent;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UStaticMeshComponent> StaticMeshComponent;
+};
